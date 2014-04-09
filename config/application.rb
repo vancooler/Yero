5ccdd4ac0cf95acdcde6cfc937f894d43f491ca7
Vincent Chu <c.tnecniv@gmail.com>
2014-04-09 13:26:03 -0700

    configured secrets and changed some environment settings

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,12 @@ module PurpleOctopus
     # config.i18n.default_locale = :de
 
     config.assets.precompile += %w( vendor/modernizr )
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
