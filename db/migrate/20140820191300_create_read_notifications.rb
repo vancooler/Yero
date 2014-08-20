53fd0845d1c2c24e66724d97efbdef18a8f6f26d
Alex <lyosha85@gmail.com>
2014-08-20 13:29:26 -0700

    set read-notification to default to false in db

--- db/migrate/20140820191300_create_read_notifications.rb
+++ db/migrate/20140820191300_create_read_notifications.rb
@@ -2,7 +2,7 @@ class CreateReadNotifications < ActiveRecord::Migration
   def change
     create_table :read_notifications do |t|
       t.references :user, index: true
-      t.boolean :before_sending_whisper_notification
+      t.boolean :before_sending_whisper_notification, default: false
 
       t.timestamps
     end
