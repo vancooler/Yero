4c29d87ab45943d324257aea0b544ddb9482b06c
Alex <lyosha85@gmail.com>
2014-08-21 15:50:30 -0700

    change temp notification

--- app/controllers/temp_notifications_controller.rb
+++ app/controllers/temp_notifications_controller.rb
@@ -18,10 +18,10 @@ class TempNotificationsController < ApplicationController
     token = "<443e69367fbbbce9c722fdf392f72af2111bde5626a916007d97382687d4b029>"
     # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
     notification = Houston::Notification.new(device: token)
-    notification.alert = "Hi YJ!!! Have a great weekend!"
+    notification.alert = "{name, message: "",}"
 
     # Notifications can also change the badge count, have a custom sound, have a category identifier, indicate available Newsstand content, or pass along arbitrary data.
-    notification.badge = 57
+    notification.badge = 1
     notification.sound = "sosumi.aiff"
     notification.category = "INVITE_CATEGORY"
     notification.content_available = true
