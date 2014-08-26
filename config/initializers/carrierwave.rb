51bd1e37c74c821eabaf1b9579b1f8265e9b825b
Alex <lyosha85@gmail.com>
2014-08-26 13:22:27 -0700

    store test files in development bucket on amazon

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,13 @@
 CarrierWave.configure do |config|
+
+  fog_dir = Rails.env == 'production' ? ENV['S3_BUCKET_NAME'] : 'yero-development'
+
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],     # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], # required
     # :region                 => 'us-west-2'
   }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']                   # required
+  config.fog_directory  = fog_dir                   # required
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
