c9b26d08eeaeb270d5db11af086fd42296c4a5dc
Liu, Yang <yang@aglobalway.com>
2014-09-16 11:46:35 -0700

    user/show avatar format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,21 @@ class UsersController < ApplicationController
       latitude:current_user.latitude,
       longitude:current_user.longitude,
       avatars: {
-        avatar_0: current_user.main_avatar.avatar.url,
-        avatar_1: current_user.user_avatars.count > 1 ? current_user.secondary_avatars.first.avatar.url : " ",
-        avatar_2: current_user.user_avatars.count > 2 ? current_user.secondary_avatars.last.avatar.url : " ",
+        avatar_0: {
+          avatar: current_user.main_avatar.avatar.url,
+          avatar_id: current_user.main_avatar.id,
+          default: true
+        },
+        avatar_1: {
+          avatar: current_user.user_avatars.count > 1 ? current_user.secondary_avatars.first.avatar.url : " ",
+          avatar_id: current_user.user_avatars.count > 1 ? current_user.secondary_avatars.first.id : " ",
+          default: false
+        },
+        avatar_2: {
+          avatar: current_user.user_avatars.count > 2 ? current_user.secondary_avatars.last.avatar.url : " ",
+          avatar_id: current_user.user_avatars.count > 2 ? current_user.secondary_avatars.last.id : " ",
+          default: false
+        }
       }
     }
 
