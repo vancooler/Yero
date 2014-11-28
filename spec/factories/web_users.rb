ecd461453a3e074d5ab33a7301b1bec5b3e49fad
Liu, Yang <yang@aglobalway.com>
2014-11-28 10:46:26 -0800

    no message

--- spec/factories/web_users.rb
+++ spec/factories/web_users.rb
@@ -1,19 +1,19 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :web_user do
-    venue nil
-    first_name "MyString"
-    last_name "MyString"
-    business_name "MyString"
-    email "MyString"
-    address_line_1 "MyString"
-    address_line_2 "MyString"
-    city "MyString"
-    state "MyString"
-    country "MyString"
-    zipcode "MyString"
-    business_phone "MyString"
-    cell_phone "MyString"
-  end
-end
+#FactoryGirl.define do
+#  factory :web_user do
+#    venue nil
+#    first_name "MyString"
+#    last_name "MyString"
+#    business_name "MyString"
+#    email "MyString"
+#    address_line_1 "MyString"
+#    address_line_2 "MyString"
+#    city "MyString"
+#    state "MyString"
+#    country "MyString"
+#    zipcode "MyString"
+#    business_phone "MyString"
+#    cell_phone "MyString"
+#  end
+#end
