fba6ea3330e793316ed3eaaf88570c7ec03e06eb
Donald Lee <donaldlee50@gmail.com>
2014-12-11 11:42:28 -0800

    YJ asked to add email, snapchat_id, wechat_id, and password to users table.

--- db/migrate/20141211191705_add_email_snapchat_wechat_password_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddEmailSnapchatWechatPasswordToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :email, :string, unique: true, null: false
-  	add_column :users, :snapchat_id, :string
-  	add_column :users, :wechat_id, :string
-  	add_column :users, :password, :string, null: false
-  end
-end
