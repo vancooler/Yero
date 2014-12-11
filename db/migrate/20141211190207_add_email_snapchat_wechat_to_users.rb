6a86971376b9c5da594a3e4aed597a090df96675
Donald Lee <donaldlee50@gmail.com>
2014-12-11 11:14:34 -0800

    revert commit

--- db/migrate/20141211190207_add_email_snapchat_wechat_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailSnapchatWechatToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :email, :string
-  	add_column :users, :snapchat_id, :string
-  	add_column :users, :wechat_id, :string
-  end
-end
