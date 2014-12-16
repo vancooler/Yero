6edacc49a0637c6b615a64b1210f247da9eeb785
Donald Lee <donaldlee50@gmail.com>
2014-12-16 10:52:52 -0800

    login api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -310,7 +310,7 @@ class UsersController < ApplicationController
 
   def login
     user = User.find_by_key(params[:key])
-    puts params[:password].inspect
+    puts login_params[:password].inspect
     # if(params[:enter] == user.email and params[:password] == user.password)
     # if user
     #   render success(user.to_json(true))
