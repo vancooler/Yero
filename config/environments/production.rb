52ea775a4028c8d77d8d17101ba9da32a435ba7e
Donald Lee <donaldlee50@gmail.com>
2014-12-17 15:59:19 -0800

    Environment configurations for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.default_url_options = { host: 'http://purpleoctopus-staging.herokuapp.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -84,5 +88,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://purpleoctopus-staging.herokuapp.com/' }
+  
 end
