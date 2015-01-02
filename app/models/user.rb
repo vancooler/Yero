3cb99791d8cafcf171caa9c1ffb72ee30b3cce87
Donald Lee <donaldlee50@gmail.com>
2015-01-02 11:41:03 -0800

    w

--- app/models/user.rb
+++ app/models/user.rb
@@ -191,8 +191,8 @@ class User < ActiveRecord::Base
   end
 
   def whisper_friends
-    active_users_id = WhisperNotification.find_friends(self.id)
-    users = User.where(id: active_users_id)
+    users = WhisperNotification.find_friends(self.id)
+    # users = User.where(id: active_users_id)
     return users
   end
 
