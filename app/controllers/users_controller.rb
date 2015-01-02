5daa7c272641d8f60fa630ba1d1e96a4d42c2046
Donald Lee <donaldlee50@gmail.com>
2015-01-02 13:49:04 -0800

    w

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -183,7 +183,7 @@ class UsersController < ApplicationController
 
       json.array! return_users do |user|
         puts "return_users:"
-        puts user["target_user"]["id"].inspect
+        puts user["target_user"].inspect
         avatar_array = Array.new
         avatar_array[0] = {
           thumbnail: user["target_user_thumb"],
@@ -199,7 +199,7 @@ class UsersController < ApplicationController
         end
 
         json.same_venue_badge          current_user.same_venue_as?(user["target_user"]["id"])
-        json.different_venue_badge     current_user.different_venue_as?(user.id) 
+        json.different_venue_badge     current_user.different_venue_as?(user["target_user"]["id"]) 
         json.actual_distance           current_user.actual_distance(user)
         json.id             user.id
         json.first_name     user.first_name
