12169d7069b7c86d27dbfafdb2efa60effea63b1
Donald Lee <donaldlee50@gmail.com>
2015-01-07 11:35:33 -0800

    venue avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -219,8 +219,8 @@ class UsersController < ApplicationController
       json.array! return_venues.each do |venue|
         venue_obj = Venue.find(venue["venue_id"])
         venue_avatar = VenueAvatar.find_by_venue_id(venue["venue_id"])
-        puts venue_avatar.inspect
         json.venue_name venue_obj["name"]
+        json.venue_avatar venue_avatar["avatar"]
         json.venue_message "Welcome to "+venue_obj["name"]+"! Open this Whisper to learn more about tonight."
         json.timestamp venue["timestamp"]
         json.accepted venue["accepted"]
