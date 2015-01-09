72900e4cf64bd3690e2462e697cbfeda5e16ab86
Michael LaRoy <mike.laroy@gmail.com>
2015-01-08 17:33:21 -0800

    sticky header on top bar for venues page, small screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,25 @@
 
 $(function(){ $(document).foundation(); });
 
+var header = $('.for-venues .contain-to-grid');
+
+if($(window).width() < 768  ){
+	header.addClass('sticky fixed')
+	$('body').addClass('f-topbar-fixed');
+}
+
+
+$(window).resize(function(event) {
+	/* Act on the event */
+
+	if($(window).width() < 768  ){
+		header.addClass('sticky fixed');
+		$('body').addClass('f-topbar-fixed');
+	}else{
+		header.removeClass('sticky fixed');
+		$('body').removeClass('f-topbar-fixed');
+	}
+});
 
 // console.log('I\'m here!');
 
