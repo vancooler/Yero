4d60728cf90b34b8a02a75a5e946380adb9022af
Liu, Yang <yang@aglobalway.com>
2015-01-09 15:11:24 -0800

    better validator

--- app/models/early_venue.rb
+++ app/models/early_venue.rb
@@ -1,4 +1,4 @@
 class EarlyVenue < ActiveRecord::Base
 	validates_presence_of :username, :venue_name, :city, :phone, :email, :job_title
-	validates :phone, inclusion: { in: 0..9 }
+	validates :phone, :numericality => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,7 @@ Rails.application.routes.draw do
   get 'about', to: 'home#about'
   get 'terms-of-use', to: 'home#terms-of-use'
   get 'careers', to: 'home#careers'
-  get 'get-in-touch', to: 'early_venues#new'
+  get 'get-in-touch', to: 'early_venues#new', as: :get_in_touch
   get 'venue-login', to: 'home#venue-login'
   get 'android', to: 'beta_signup_users#android'
   get 'beta-signup', to: 'beta_signup_users#beta'
