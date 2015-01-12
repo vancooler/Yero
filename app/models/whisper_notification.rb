e4c034e8fd7c44896d087559e21588a126f87c06
Donald Lee <donaldlee50@gmail.com>
2015-01-12 14:24:16 -0800

    Notification message.

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -424,7 +424,7 @@ class WhisperNotification < AWS::Record::HashModel
     p 'The notification data'
     p notification.inspect  
     # And... sent! That's all it takes.
-    p apn.push(notification)
+    apn.push(notification)
 
   end
 
