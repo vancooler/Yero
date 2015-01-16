f132b13553dfd91ce19746f4c291082158168e50
Donald Lee <donaldlee50@gmail.com>
2015-01-15 16:08:13 -0800

    work

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -110,6 +110,8 @@ class WhisperNotification < AWS::Record::HashModel
         if target_id > 0
           user = User.find(target_id)
           h['target_user'] = user
+          p 'THE USER:'
+          p user.inspect
           if user.main_avatar
             h['avatars'] = Array.new
             h['avatars']['target_user_thumb'] = user.main_avatar.avatar.thumb.url
