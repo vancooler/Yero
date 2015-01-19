a2c8de3e44929a8991245c8f25420a4df95e7468
Donald Lee <donaldlee50@gmail.com>
2015-01-19 14:50:26 -0800

    duplicate email

--- app/controllers/whispers_controller.rb
+++ app/controllers/whispers_controller.rb
@@ -156,6 +156,9 @@ class WhispersController < ApplicationController
       if params[:accepted].to_i == 1
         state = 'accepted'
         n = WhisperNotification.find_whisper(whisperId, state)
+        p 'find'
+        p n.inspect
+        n.send_accept_notification_to_target_user(message)
         render json: success
       elsif params[:declined].to_i == 1
         state = 'declined'
