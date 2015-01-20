151ba3f38cf28833c37bba8f0ab0da004f0038b4
Donald Lee <donaldlee50@gmail.com>
2015-01-19 16:14:45 -0800

    Accept whisper notifications

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -503,7 +503,7 @@ class WhisperNotification < AWS::Record::HashModel
     apn.push(notification)
   end
 
-  def send_accept_notification_to_sender(hash)
+  def self.send_accept_notification_to_sender(hash)
     #this shall be refactored once we have more phones to test with
     app_local_path = Rails.root
     p hash.inspect
