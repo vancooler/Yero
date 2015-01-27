12ede6bcdc57f37a25d939f17254801f6fcb3c40
Donald Lee <donaldlee50@gmail.com>
2015-01-26 16:54:28 -0800

    gemlock

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -324,8 +324,9 @@ class WhisperNotification < AWS::Record::HashModel
     end
     users = Array.new
     # users = target_user_array + origin_user_array
-    users = target_user_array.zip(origin_user_array).flatten.compact
-    users = users.sort_by { |hsh| hsh[:timestamp] }
+    # users = target_user_array.zip(origin_user_array).flatten.compact
+    # users = users.sort_by { |hsh| hsh[:timestamp] }
+    users = (target_user_array + origin_user_array).sort_by(&:timestamp)
     return users
   end
 
