ba85f12d19ba99f9fb300d6e7031f587cc3bb433
Donald Lee <donaldlee50@gmail.com>
2015-01-30 13:11:11 -0800

    reported_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -180,7 +180,7 @@ class UsersController < ApplicationController
       puts runtime.inspect
       logger.info "NEWTIME: " + diff_1.to_s 
       puts "collected whispers:"
-      puts collect_whispers.inspect
+      puts collected_whispers.inspect
     else
       users = ActiveInVenueNetwork.count
     end
