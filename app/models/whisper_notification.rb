340b9d91557512f9474bcb17b476c4ef6330afc3
Donald Lee <donaldlee50@gmail.com>
2015-01-30 12:47:00 -0800

    reported_user

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -422,6 +422,9 @@ class WhisperNotification < AWS::Record::HashModel
     table.load_schema
     items = table.items.where(:origin_id).equals(current_user.id.to_s).where(:notification_type).equals("2").where(:created_date).equals(Date.today.to_s)
     items = items.select(:target_id) {|data| p data.attributes["target_id"]}
+    p "items"
+    p items
+    break
     if items.present? and items.count > 0
       return items
     else
