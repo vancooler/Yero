85abd0e3611c24cfe039df37ba7e220c06789d1d
Donald Lee <donaldlee50@gmail.com>
2015-01-30 13:46:01 -0800

    reported_user

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -421,7 +421,7 @@ class WhisperNotification < AWS::Record::HashModel
     table = dynamo_db.tables['WhisperNotification']
     table.load_schema
     items = table.items.where(:origin_id).equals(current_user.id.to_s).where(:notification_type).equals("2").where(:created_date).equals(Date.today.to_s)
-    items = items.select(:target_id) {|data| p data.attributes["target_id"]}
+    # items = items.select(:target_id) {|data| p data.attributes["target_id"]}
     if items.present? and items.count > 0
       return items
     else
