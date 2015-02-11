8051835fb92c88d6ed75420042947d22aa384157
Donald Lee <donaldlee50@gmail.com>
2015-02-10 16:22:31 -0800

    Changes made to distance

--- app/models/user.rb
+++ app/models/user.rb
@@ -174,7 +174,7 @@ class User < ActiveRecord::Base
     end
 =end
     # users = User.where(id: active_users_id) #Find all the users with the id's in the array.
-    user = User.near(self, 50, :units => :km)
+    users = User.near(self, 50, :units => :km)
     if !gender.nil? || gender != "A"
       if gender == "M" or gender == "F"
         users = users.where(:gender => gender) #Filter by gender
