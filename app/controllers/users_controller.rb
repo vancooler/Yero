74242ccde5c93c9e42249e976451a0dabd0caaa4
Donald Lee <donaldlee50@gmail.com>
2015-02-12 16:27:21 -0800

    apps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -322,7 +322,7 @@ class UsersController < ApplicationController
     
     user = user_registration.user
 
-    if user_registration.create
+    if ur = user_registration.create
       # #signup with the avatar id
       # avatar_id = sign_up_params[:avatar_id]
       # response = user.to_json(true)
@@ -359,8 +359,8 @@ class UsersController < ApplicationController
       # render json: user_avatar.to_json.inspect
       # 
       
-      # n = WhisperNotification.create_in_aws(user_registration.id, "SYSTEM", 0, 1, intro)
-      # n.welcome_notification_from_yero
+      n = WhisperNotification.create_in_aws(ur.id, "SYSTEM", 0, 1, intro)
+      n.welcome_notification_from_yero
 
       render json: success(response)
     else
