23e7e390bb3509a0c445b34d241af223d28978a2
Donald Lee <donaldlee50@gmail.com>
2015-02-23 16:35:33 -0800

    Installed clockwork

--- app/models/user_friends.rb
+++ app/models/user_friends.rb
@@ -14,4 +14,48 @@ class UserFriends < AWS::Record::HashModel
 		n.save!
 		return n
 	end
+
+	def return_friends(current_user_id)
+		dynamo_db = AWS::DynamoDB.new # Make an AWS DynamoDB object
+	    table = dynamo_db.tables['UserFriends'] # Choose the 'WhisperNotification' table
+	    table.load_schema 
+	    friends = table.items.where(:user_id).equals(current_user_id)
+	    friends_array = Array.new
+	    if friends and friends.count > 0
+	    	friends.each do |friend|
+	    		attributes = friend.attributes.to_h
+        		friend_id = attributes['friend_id'].to_i
+	    		h = Hash.new
+	    		if friends_array.include? friend_id
+		          p 'in the array'
+		        else
+		          if friend_id > 0
+		            user = User.find(friend_id)
+		            h['target_user'] = user
+		            if user.main_avatar
+		              h['target_user_thumb'] = user.main_avatar.avatar.thumb.url
+		              h['target_user_main'] = user.main_avatar.avatar.url
+		              if user.secondary_avatars
+		                h['target_user_secondary1'] = user.user_avatars.count > 1 ? user.secondary_avatars.first.avatar.url : ""
+		                h['target_user_secondary2'] = user.user_avatars.count > 2 ? user.secondary_avatars.last.avatar.url : ""
+		              end
+		            end
+		          else
+		            h['target_user'] = ''
+		          end
+		          h['timestamp'] = attributes['timestamp'].to_i
+		          h['accepted'] = attributes['accepted'].to_i
+		          h['declined'] = attributes['declined'].to_i
+		          h['whisper_id'] = attributes['id']
+		          friends_array << h  
+		          users = Array.new
+		          users = friends_array
+		          users = users.sort_by { |hsh| hsh[:timestamp] }
+		          p "users#afd"
+		          p users.inspect
+		          return users.reverse
+		        end 
+	    	end
+	    end
+	end
 end
