8bc1c61f2c78e5cc085935d78802838030c391d0
Donald Lee <donaldlee50@gmail.com>
2015-02-23 16:05:29 -0800

    Installed clockwork

--- app/models/user_friends.rb
+++ app/models/user_friends.rb
@@ -1,4 +1,10 @@
 class UserFriends < AWS::Record::HashModel
+	integer_attr :user_id
+	integer_attr :friend_id
+
+	integer_attr :timestamp
+	string_attr :created_date
+
 	def self.create_in_aws(user1_id, user2_id)
 		n = UserFriends.new
 		n.user_id = user1_id
