bd0291ad2a906b8d78b61d7e5eb3a9a4dabc12ac
Donald Lee <donaldlee50@gmail.com>
2015-02-26 13:27:33 -0800

    A user should be able to see other users (with different venue badge) if he is not within a beacon (i.e. at home).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -185,6 +185,8 @@ class UsersController < ApplicationController
       end
       
       # users = users - same_beacon_users - same_venue_users # Split out the users such that users only contain those that are not in the same venue or same beacon
+      p "Current user exclusivity"
+      p current_user.exclusive 
       if !!current_user.exclusive == true
         users = same_venue_users.sort_by { |hsh| hsh[:actual_distance] }
       else
