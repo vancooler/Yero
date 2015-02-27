6eb87d351da026ca5a788ab11bf267171719549a
Donald Lee <donaldlee50@gmail.com>
2015-02-27 11:13:00 -0800

    Friends

--- app/models/user_friends.rb
+++ app/models/user_friends.rb
@@ -18,20 +18,15 @@ class UserFriends < AWS::Record::HashModel
 	def self.return_friends(current_user_id)
 		dynamo_db = AWS::DynamoDB.new # Make an AWS DynamoDB object
 	    table = dynamo_db.tables['UserFriends'] # Choose the 'WhisperNotification' table
-	    table.load_schema 
-	    p "The current user id is"
-	    p current_user_id
-	    friends = table.items.where(:user_id).equals(current_user_id)
+	    table.load_schema # Load the table
+	    friends = table.items.where(:friend_id).equals(current_user_id)
 	    friends_array = Array.new
 	    if friends and friends.count > 0
 	    	friends.each do |friend|
 	    		attributes = friend.attributes.to_h
         		friend_id = attributes['friend_id'].to_i
 	    		h = Hash.new
-	    		if friends_array.include? friend_id
-		          p 'in the array'
-		        else
-		          if friend_id > 0
+	    		if friend_id > 0
 		            user = User.find(friend_id)
 		            h['intro'] = user.introduction_1
 		            h['target_user'] = user
@@ -43,20 +38,18 @@ class UserFriends < AWS::Record::HashModel
 		                h['target_user_secondary2'] = user.user_avatars.count > 2 ? user.secondary_avatars.last.avatar.url : ""
 		              end
 		            end
-		          else
+		        else
 		            h['target_user'] = ''
-		          end
+		        end
 		          h['timestamp'] = attributes['timestamp'].to_i
-		          
-		          friends_array << h  
-		          users = Array.new
-		          users = friends_array
-		          users = users.sort_by { |hsh| hsh[:timestamp] }
-		          p "users#afd"
-		          p users.inspect
-		          return users.reverse
-		        end 
+		          friends_array << h     
 	    	end
+	    	users = Array.new
+	        users = friends_array
+	        users = users.sort_by { |hsh| hsh[:timestamp] }
+	        p "users#afd"
+	        p users.inspect
+	        return users.reverse
 	    end
 	end
 end
