29766118d48e8abe93b3f3edd8fd5895dc183753
Donald Lee <donaldlee50@gmail.com>
2015-02-27 16:04:19 -0800

    YJ changes for introduction_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -703,6 +703,7 @@ class UsersController < ApplicationController
         json.snapchat_id  user["target_user"]["snapchat_id"]
         json.wechat_id  user["target_user"]["wechat_id"]
         json.timestamp  user["timestamp"]
+        json.timestamp_read  user["timestamp_read"]
         json.accepted   user["accepted"].blank? ? nil : user["accepted"]
         json.declined   user["declined"].blank? ? nil : user["declined"]
         json.whisper_id  user["whisper_id"].blank? ? nil : user["whisper_id"]
