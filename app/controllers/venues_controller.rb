343cd1f9b35683c2b3b4670118e980b1f58ad853
Donald Lee <donaldlee50@gmail.com>
2015-03-04 10:48:36 -0800

    times

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -16,11 +16,11 @@ class VenuesController < ApplicationController
     places.each do |p|
       puts "P is: "
       puts p.inspect
-      puts p["city"]
+      
       # Get time
-      time = Net::HTTP.get_response(URI.parse("http://api.timezdb.com/?q=getByCityName&apiKey="+ENV['TIMEZDB_API_KEY']+"&cityName="+p.city)).body
+      # time = Net::HTTP.get_response(URI.parse("http://api.timezdb.com/?q=getByCityName&apiKey="+ENV['TIMEZDB_API_KEY']+"&cityName="+p.city)).body
       puts "Time time"
-      puts time
+      # puts time
 
     end
     render nothing: true 
