f4eb15d08bde08a8ec5b74c1662b7d6eb3ed32f2
Donald Lee <donaldlee50@gmail.com>
2015-03-05 15:36:47 -0800

    re

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -654,7 +654,7 @@ class UsersController < ApplicationController
     times_array = Array.new
     times_result.each do |timezone|
       puts "Time time at timezone:"
-      puts timezone.time
+      puts timezone["timezone"].time
       open_network_tz = Array(timezone, timezone.time)
       puts "The open_network"
       puts open_network_tz.inspect
