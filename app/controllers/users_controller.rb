49dd47cf026b5a023628bea2ce59af3d84d52443
Donald Lee <donaldlee50@gmail.com>
2015-03-05 14:39:21 -0800

    re

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -650,7 +650,7 @@ class UsersController < ApplicationController
 
   def network_open
     p "Time zone is:"
-    times = Timezone.new
+    times = TimeZonePlace.new
     p times
     # times_result = times.find(:all)
     # times_result.each do |time|
