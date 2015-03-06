f34e9fa4ce9e9fff92c52c80d263a8ce340f487e
Donald Lee <donaldlee50@gmail.com>
2015-03-06 11:17:28 -0800

    re

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -654,9 +654,11 @@ class UsersController < ApplicationController
     times_array = Array.new
     times_result.each do |timezone|
       Time.zone = timezone["timezone"]
+      open_network_tz = [Time.zone.to_s, Time.zone.now.strftime("%H:%M")]
+      puts open_network_tz.inspect
       if Time.zone.now.strftime("%H:%M") == "17:00"
-        open_network_tz = [Time.zone.to_s, Time.zone.now.strftime("%H:%M")]
-        puts open_network_tz.inspect
+        puts "5pm in:"
+        puts Time.zone.to_s
       end
     end
     render nothing: true 
