243e51901ac2e3788af3d56733b6dca29e2369be
Donald Lee <donaldlee50@gmail.com>
2015-03-05 16:46:46 -0800

    re

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -653,6 +653,7 @@ class UsersController < ApplicationController
     times_result = TimeZonePlace.connection.select_all("select timezone from time_zone_places")
     times_array = Array.new
     times_result.each do |timezone|
+      puts timezone
       Time.zone = timezone["timezone"]
       open_network_tz = [Time.zone.to_s, Time.zone.now.strftime("%H:%M")]
       puts open_network_tz.inspect
