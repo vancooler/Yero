2ea4969071e7aef652f8b511a102784a9c988f49
Donald Lee <donaldlee50@gmail.com>
2015-03-05 16:51:06 -0800

    re

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -653,9 +653,9 @@ class UsersController < ApplicationController
     times_result = TimeZonePlace.connection.select_all("select timezone from time_zone_places")
     times_array = Array.new
     times_result.each do |timezone|
-      puts timezone[:timezone]
+      puts timezone[0]
       Time.zone = timezone["timezone"]
-      open_network_tz = [timezone[:timezone].to_s, Time.zone.now.strftime("%H:%M")]
+      open_network_tz = [timezone[0].to_s, Time.zone.now.strftime("%H:%M")]
       puts open_network_tz.inspect
     end
     render nothing: true 
