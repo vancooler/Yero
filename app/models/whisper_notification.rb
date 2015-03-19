7ba528090f7d63e2778b5ea07ad69c523c498dc1
Donald Lee <donaldlee50@gmail.com>
2015-03-19 13:55:02 -0700

    whisper restrictions

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -453,7 +453,7 @@ class WhisperNotification < AWS::Record::HashModel
     table.load_schema
     items = table.items.where(:target_id).equals(target_user_id.to_s).where(:origin_id).equals(origin_user_id.to_s).where(:notification_type).equals("2").where(:created_date).equals(Date.today.to_s)
     puts "whisper sent"
-    puts items.to_h.inspect
+    puts items.attributs.to_h.inspect
     if items.present? and items.count > 0
       return true
     else
