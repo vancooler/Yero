ee4eff3914fbcd92dc02d7257cc5ef1ef51ccc0c
Donald Lee <donaldlee50@gmail.com>
2015-03-23 14:56:11 -0700

    privacy and terms of use

--- app/models/user.rb
+++ app/models/user.rb
@@ -320,17 +320,16 @@ class User < ActiveRecord::Base
     times_array = Array.new # Make a new array to hold the times that are at 5:00pm
     times_result.each do |timezone| # Check each timezone
       Time.zone = timezone["timezone"] # Assign timezone
-      # if Time.zone.now.strftime("%H:%M") == "17:00" # If time is 17:00
+      if Time.zone.now.strftime("%H:%M") == "17:00" # If time is 17:00
         open_network_tz = [Time.zone.name.to_s, Time.zone.now.strftime("%H:%M")] #format it
         times_array << open_network_tz #Throw into array
-      # end
+      end
     end
-    people_array = Array.new
-    puts "The open network" 
+    people_array = Array.new 
     
     times_array.each do |timezone| #Each timezone that we found to be at 17:00
       usersInTimezone = UserLocation.find_by_dynamodb_timezone(timezone[0]) #Find users of that timezone
-      puts timezone[0]
+      
       if !usersInTimezone.blank? # If there are people in that timezone
         usersInTimezone.each do |user|
           attributes = user.attributes.to_h # Turn the people into usable attributes
