4969d77cd0fe0027eb544cac157122b4f653ff37
Donald Lee <donaldlee50@gmail.com>
2015-03-25 10:48:21 -0700

    privacy and terms of use

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -432,6 +432,7 @@ class WhisperNotification < AWS::Record::HashModel
     table.load_schema
     items = table.items.where(:target_id).equals(target_user_id.to_s).where(:origin_id).equals(origin_user_id.to_s).where(:notification_type).equals("2").where(:created_date).equals(Date.today.to_s)
     puts "whisper sent"
+    puts items.inspect
     if items.count == 1
       items.each do |i|
         hash = i.attributes.to_h
