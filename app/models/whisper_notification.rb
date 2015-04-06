4cfa334332fe02f0b7dc4841e9529f4fb6c6fdb3
Liu, Yang <yang@aglobalway.com>
2015-04-06 10:00:59 -0700

    whisper check sent in 12 hours

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -411,11 +411,13 @@ class WhisperNotification < AWS::Record::HashModel
     return true
   end
 
+  # Function that gets all the users received whisper from current user
   def self.collect_whispers(current_user)
     dynamo_db = AWS::DynamoDB.new
     table = dynamo_db.tables['WhisperNotification']
     table.load_schema
-    items = table.items.where(:origin_id).equals(current_user.id.to_s).where(:notification_type).equals("2").where(:created_date).equals(Date.today.to_s)
+    timestamp = Time.now
+    items = table.items.where(:origin_id).equals(current_user.id.to_s).where(:notification_type).equals("2").where(:timestamp).gte(timestamp - 12*3600)
     return_array = Array.new
     items.each do |p|
       return_array << p.hash_value
