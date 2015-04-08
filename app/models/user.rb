54304c7439f0f9059e9ff828428c1206815f2fd5
Donald Lee <donaldlee50@gmail.com>
2015-04-07 17:21:40 -0700

    funny

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   has_one :read_notification, dependent: :destroy
   has_one :active_in_venue, dependent: :destroy
   has_one :active_in_venue_network, dependent: :destroy
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 5 }
 
   reverse_geocoded_by :latitude, :longitude
 
