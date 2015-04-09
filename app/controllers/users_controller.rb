b5380427e9e1ce22970150f0119269c6c7bb7139
Donald Lee <donaldlee50@gmail.com>
2015-04-09 16:15:59 -0700

    password changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -620,37 +620,37 @@ class UsersController < ApplicationController
       puts "params[:user].blank"
       puts @user.inspect
       if @user.email.to_s.downcase != params[:user][:email].to_s.downcase 
-        flash[:danger] = "Email given does not match email from password recovery."
+        flash[:danger][:email_mismatch] = "Email given does not match email from password recovery."
         puts "Email given does not match email from password recovery."
         email_mismatch = false
       end
       if params[:user][:email].blank?
-        flash[:danger] = "Email cannot be blank."
+        flash[:danger][:email_blank] = "Email cannot be blank."
         puts "Email cannot be blank."
         email_blank = false
       end
       if params[:user][:password].blank?
-        flash[:danger] = "Password cannot be empty."
+        flash[:danger][:password_blank] = "Password cannot be empty."
         puts "Password cannot be empty."
         password_blank = false
       end
       if params[:user][:password_confirmation].blank?
-        flash[:danger] = "Password confirmation cannot be empty."
+        flash[:danger][:password_conf_empty] = "Password confirmation cannot be empty."
         puts "Password confirmation cannot be empty."
         password_conf_empty = false
       end
       if params[:user][:password].length < 6
-        flash[:danger] = "Password is too short (minimum is 6 characters)."
+        flash[:danger][:password_short] = "Password is too short (minimum is 6 characters)."
         puts "Password is too short (minimum is 6 characters)."
         password_short = false
       end
       if params[:user][:password] != params[:user][:password_confirmation]
-        flash[:danger] = "Passwords do not match."
+        flash[:danger][:password_mismatch] = "Passwords do not match."
         puts "Passwords do not match."
         password_mismatch = false
       end
       if !params[:user][:email].match /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-        flash[:danger] = "Email is invalid" 
+        flash[:danger][:email_invalid] = "Email is invalid" 
         puts "Email is invalid"
         email_invalid = false
       end
