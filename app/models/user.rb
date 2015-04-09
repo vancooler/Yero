6d5f84b2c0beb2d0d77f208d696429c80c673ecf
Donald Lee <donaldlee50@gmail.com>
2015-04-09 14:22:25 -0700

    error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }, presence: true
+  validates :password, presence: true
 
 
   reverse_geocoded_by :latitude, :longitude
