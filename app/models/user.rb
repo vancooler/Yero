92caa3bfcbfba5b3029af771a6a8dfef93292357
Liu, Yang <yang@aglobalway.com>
2015-04-13 15:30:51 -0700

    implemented bug commented filter for is_connected

--- app/models/user.rb
+++ app/models/user.rb
@@ -153,8 +153,10 @@ class User < ActiveRecord::Base
 
     # users = User.where(id: active_users_id) #Find all the users with the id's in the array.
     max_distance = max_distance.blank? ? 20 : max_distance+1 # Do max_distance+1 to include distance ranges (i.e. 9-10km, people 10km are included)
-    # only return users with avatar near current user
+    # only return users with avatar near current user 
     users = User.includes(:user_avatars).where.not(user_avatars: { id: nil }).where(user_avatars: { is_active: true}).where(user_avatars: { default: true}).near(self, max_distance, :units => :km)
+    # filter for is_connected 
+    # users = User.includes(:user_avatars).where(is_connected: true).where.not(user_avatars: { id: nil }).where(user_avatars: { is_active: true}).where(user_avatars: { default: true}).near(self, max_distance, :units => :km)
 
     if !gender.nil? || gender != "A"
       if gender == "M" or gender == "F"
