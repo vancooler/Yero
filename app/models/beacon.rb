c5f327a6238ee76601487da5809a103575c6f719
Donald Lee <donaldlee50@gmail.com>
2015-04-14 11:31:15 -0700

    accepts_nested_attributes_for :beacon

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -2,7 +2,7 @@ class Beacon < ActiveRecord::Base
   # A beacon has a unique ID
 
   has_many :temperatures, dependent: :destroy
-  belongs_to :room
+  belongs_to :venue
 
   before_save :default_room
 
