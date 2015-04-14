18f0fd084a9e2b45419b81cfe9671aef431f88be
Liu, Yang <yang@aglobalway.com>
2015-04-14 10:38:55 -0700

    venue filter

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -80,8 +80,8 @@ class VenuesController < ApplicationController
       distance = 10000
     end
     user = User.find_by_key(params[:key])
-    venues = Venue.all
-    # venues = Venue.near_venues(user, distance)
+    # venues = Venue.all
+    venues = Venue.near_venues(user, distance)
 
     # if params[:after]
     #   new_list = []
