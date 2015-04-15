5ba9a242cc67cb29f99f36fb05bed4ccdd52bf0d
Liu, Yang <yang@aglobalway.com>
2015-04-15 11:56:40 -0700

    render first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -314,6 +314,8 @@ class UsersController < ApplicationController
     puts "The Adjust time is: "
     puts runtime.inspect
 
+    render json: success(users, "data")
+    
     WhisperNotification.viewed_by_sender(whispers_array)
     
     time_3 = Time.now
@@ -321,7 +323,6 @@ class UsersController < ApplicationController
     puts "The Update time is: "
     puts runtime.inspect
 
-    render json: success(users, "data")
   end
 
 
