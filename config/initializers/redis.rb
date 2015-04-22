6d1ccf2795f8a3f9c377d8f4cc5572b111da8452
Liu, Yang <yang@aglobalway.com>
2015-04-22 13:53:28 -0700

    no message

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,3 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 # REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
