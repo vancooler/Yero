affd7a59987ee547fe5a556fff8300b1978d6a62
Liu, Yang <yang@aglobalway.com>
2015-05-01 15:39:42 -0700

    change back to 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,7 +79,7 @@ class UsersController < ApplicationController
     page_number = params[:page] if !params[:page].blank?
     users_per_page = params[:per_page] if !params[:per_page].blank?
 
-    gate_number = 50
+    gate_number = 4
     result = current_user.people_list(gate_number, gender, min_age, max_age, venue_id, min_distance, max_distance, everyone, page_number, users_per_page)
     
 =begin    
