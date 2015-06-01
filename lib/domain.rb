49ae395f7e7105c77a7a929d0037ce3fb0ca0240
Liu, Yang <yang@aglobalway.com>
2015-06-01 09:50:57 -0700

    domain check

--- lib/domain.rb
+++ lib/domain.rb
@@ -3,6 +3,6 @@ class Domain
   	puts "REQQQ"
   	puts request.domain.inspect
   	puts request.env["SERVER_NAME"]
-    request.domain.present? && (request.domain.include? "purpleoctopus-staging" or request.domain.include? "localhost")
+    request.domain.present? && (request.domain.include? "herokuapp.com" or request.domain.include? "localhost")
   end
 end
