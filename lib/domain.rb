295087cfee158b8b4a0a6960e1320adf2bd19f73
Liu, Yang <yang@aglobalway.com>
2015-06-01 09:46:44 -0700

    outputs server name

--- lib/domain.rb
+++ lib/domain.rb
@@ -1,7 +1,8 @@
 class Domain
   def self.matches?(request)
   	puts "REQQQ"
-  	puts request.inspect
+  	puts request.domain.inspect
+  	puts request.env["SERVER_NAME"]
     request.domain.present? && (request.domain.include? "purpleoctopus-staging" or request.domain.include? "localhost")
   end
 end
