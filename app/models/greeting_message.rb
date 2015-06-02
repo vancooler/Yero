d34816e6814757a1b90c1a261cfaf0414efba432
Liu, Yang <yang@aglobalway.com>
2015-06-02 11:04:27 -0700

    greeting posters

--- app/models/greeting_message.rb
+++ app/models/greeting_message.rb
@@ -4,7 +4,7 @@ class GreetingMessage < ActiveRecord::Base
   # has_many :venue_avatars
   belongs_to :weekday
   belongs_to :venue
-  # accepts_nested_attributes_for :greeting_avatars, allow_destroy: true
+  accepts_nested_attributes_for :greeting_posters, allow_destroy: true
 
 
 end
