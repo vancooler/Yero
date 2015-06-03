5aac0ce46705c88d6c05bd6f69211ce07f822336
Liu, Yang <yang@aglobalway.com>
2015-06-02 20:35:47 -0700

    fix the bug for timezont

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -34,17 +34,20 @@ class UserLocation < AWS::Record::HashModel
     table.load_schema
     # items = table.items.where(:timezone).equals(timezone.to_s)
     items = table.items.where(:user_id).equals(user_id.to_s).select(:timezone)
+    puts "COUNT"
+    puts items.count
     if items and items.count > 0
       items.each do |user|
         attributes = user.attributes # Turn the people into usable attributes
         if !attributes["timezone"].nil? 
-          timezone =  attributes["timezone"].to_i
+          timezone =  attributes["timezone"]
         else
           timezone = 'America/Vancouver'
         end   
       end
     end
-
+    puts "timezone:::"
+    puts timezone
     # 5 am tomorrow
     expire_timestamp = DateTime.strptime(timestamp.to_s,'%s').in_time_zone(timezone).tomorrow.beginning_of_day + 5.hours
 
