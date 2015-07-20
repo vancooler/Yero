f37846fc626de6c1aabedd1f1169282162f697b9
Liu, Yang <yang@aglobalway.com>
2015-07-20 12:31:44 -0700

    password reset token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720115637) do
+ActiveRecord::Schema.define(version: 20150720123050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -386,6 +386,7 @@ ActiveRecord::Schema.define(version: 20150720115637) do
     t.boolean  "enough_user_notification_sent_tonight", default: false
     t.datetime "key_expiration"
     t.string   "line_id"
+    t.string   "password_reset_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
