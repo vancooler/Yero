0504cb598a6433be737c97c846aa605da7e4c37f
Liu, Yang <yang@aglobalway.com>
2015-07-25 18:40:57 -0700

    different venue remove campuses

--- app/models/user.rb
+++ app/models/user.rb
@@ -629,7 +629,7 @@ class User < ActiveRecord::Base
 
       campus_id = VenueType.find_by_name("Campus")
       if campus_id
-        campus_venue_ids = Venue.where(venue_type_id: campus_id.to_s).map(&:id)
+        campus_venue_ids = Venue.where(venue_type_id: campus_id.id.to_s).map(&:id)
       else
         campus_venue_ids = [nil]
       end
