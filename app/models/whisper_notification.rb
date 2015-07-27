84a623c709cabadae1eec81dae2551bd3bda68e0
Liu, Yang <yang@aglobalway.com>
2015-07-27 01:11:29 -0700

    badge number check

--- app/models/whisper_notification.rb
+++ app/models/whisper_notification.rb
@@ -1090,7 +1090,7 @@ class WhisperNotification < AWS::Record::HashModel
 
     chat_items = table.items.where(:target_id).equals(user_id.to_s).where(:notification_type).equals("2").where(:viewed).equals(0)
     greeting_items = table.items.where(:target_id).equals(user_id.to_s).where(:notification_type).equals("1").where(:viewed).equals(0)
-    accept_items = table.items.where(:target_id).equals(user_id.to_s).where(:notification_type).equals("2").where(:accepted).equals(1).where(:viewed).equals(0)
+    accept_items = table.items.where(:target_id).equals(user_id.to_s).where(:notification_type).equals("3").where(:viewed).equals(0)
 
     chat_request_number = 0
     venue_greeting_number = 0
