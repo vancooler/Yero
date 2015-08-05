008d1794aa2ec9cb01ccf55c511d7609e2c33af6
Liu, Yang <yang@aglobalway.com>
2015-08-05 12:06:30 -0700

    css changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -166,7 +166,7 @@ class ApplicationController < ActionController::Base
   def add_www_subdomain
     # puts "request"
     # puts request.path
-    unless /^www/.match(request.host)
+    unless /^www/.match(request.host) or request.host_with_port == 'localhost:3000'
       redirect_to("#{request.protocol}www.#{request.host_with_port}#{request.path}",
                   :status => 301)
     end
