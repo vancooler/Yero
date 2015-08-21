356ed9ca6f55181dea7b1cdb51e230c876a3ecc5
Liu, Yang <yang@aglobalway.com>
2015-08-21 16:39:08 -0700

    small fix

--- app/admin/user_avatar.rb
+++ app/admin/user_avatar.rb
@@ -171,7 +171,7 @@ ActiveAdmin.register UserAvatar, :as => "User Screening" do
       link = link_to avatar.origin_url, :target => "_blank" do 
         image_tag avatar.thumb_url, {:style => "height:100px;width:100px;"}
       end
-      link + ((avatar.is_active.nil? or avatar.order.nil?) ? '' : (avatar.is_active == false or avatar.order.to_i != 0 ? raw('') : raw('<span class="status_tag yes">P</span>')))
+      link + ((avatar.is_active.nil? or avatar.order.nil?) ? '' : (avatar.is_active == false or avatar.order.to_i != 0 ? '' : raw('<span class="status_tag yes">P</span>')))
     end
     column "User (ID)", :user
     column "Gender", :user do |ua|
