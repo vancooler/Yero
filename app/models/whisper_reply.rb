20dbd832bc4abad41b82ee4fb1526548992df510
Liu, Yang <yang@aglobalway.com>
2015-09-04 14:02:49 -0700

    archive whisper replies

--- app/models/whisper_reply.rb
+++ app/models/whisper_reply.rb
@@ -15,25 +15,27 @@ class WhisperReply < ActiveRecord::Base
 	      table.load_schema
 	    end
 
-	    replies.each_slice(25) do |replies_group|
-	        batch = AWS::DynamoDB::BatchWrite.new
-	        replies_array = Array.new
-	        replies_group.each do |reply|
-	          if !reply.blank?
-	            request = Hash.new()
-	            request["origin_id"] = reply.speaker_id
-	            request["whisper_dynamo_id"] = reply.whisper.dynamo_id
-	            request["target_id"] = (reply.whisper.origin_user_id == reply.speaker_id ? reply.whisper.target_user_id : reply.whisper.origin_user_id)
-	            request["timestamp"] = reply.created_at.to_i
-	            request["message"] = reply.message
-	            replies_array << request
-	          end
-	        end
-	        if replies_array.count > 0
-	          table_name = WhisperNotification.table_prefix + 'WhisperNotification'
-	          batch.put(table_name, replies_array)
-	          batch.process!
-	        end
+	    replies.each do |replies_group|
+            target_id = (reply.whisper.origin_user_id == reply.speaker_id ? reply.whisper.target_user_id : reply.whisper.origin_user_id)
+	    	item = table.items.put(:origin_id => reply.speaker_id, :timestamp => reply.created_at.to_i, :target_id => target_id, :message => reply.message, :whisper_dynamo_id => reply.whisper.dynamo_id)
+	        # batch = AWS::DynamoDB::BatchWrite.new
+	        # replies_array = Array.new
+	        # replies_group.each do |reply|
+	        #   if !reply.blank?
+	        #     request = Hash.new()
+	        #     request["origin_id"] = reply.speaker_id
+	        #     request["timestamp"] = reply.created_at.to_i
+	        #     request["whisper_dynamo_id"] = reply.whisper.dynamo_id
+	        #     request["target_id"] = (reply.whisper.origin_user_id == reply.speaker_id ? reply.whisper.target_user_id : reply.whisper.origin_user_id)
+	        #     request["message"] = reply.message
+	        #     replies_array << request
+	        #   end
+	        # end
+	        # if replies_array.count > 0
+	        #   table_name = WhisperNotification.table_prefix + 'WhisperNotification'
+	        #   batch.put(table_name, replies_array)
+	        #   batch.process!
+	        # end
 	    end
 	    
 	end
