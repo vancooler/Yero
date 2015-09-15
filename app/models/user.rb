bbb83b27cf0758efec127c6be77aa6783a672934
Liu, Yang <yang@aglobalway.com>
2015-09-15 12:52:12 -0700

    5 am cleanup for only 1.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -440,7 +440,7 @@ class User < ActiveRecord::Base
     # disconnect all users
     people_array = Array.new 
     if !times_array.empty?    
-      people_array = User.where(:timezone_name => times_array).map(&:id)
+      people_array = User.where(:timezone_name => times_array).where("version <> ? OR version is ?", '2.0', nil).map(&:id)
       # people_array = UserLocation.find_by_dynamodb_timezone(times_array, true) #Find users of that timezone
     end
     if !people_array.empty? 
