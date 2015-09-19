cf02de782370f05989dbb583c55db2f30116fefe
Liu, Yang <yang@aglobalway.com>
2015-09-19 00:31:25 -0700

    remove it

--- app/models/user_avatar.rb
+++ app/models/user_avatar.rb
@@ -61,38 +61,42 @@ class UserAvatar < ActiveRecord::Base
 
   def self.remove_from_aws(avatar_url, thumb_url)
     # remove in AWS
-    if ENV['AWS_ACCESS_KEY_ID'].blank?
-      access_key_id = 'AKIAJYFD6Q4GYZSGTX5Q'
-    end
-    if ENV['AWS_SECRET_ACCESS_KEY'].blank?
-      access_key = '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'
-    end
-    if ENV['S3_BUCKET_NAME'].blank?
-      bucket = 'yero-development'
-    end
-    AWS.config(:access_key_id => access_key_id, :secret_access_key => access_key)
-    s3 = AWS::S3.new
+    # :nocov:
+    if Rails.env == 'production'
+      if !ENV['AWS_ACCESS_KEY_ID'].blank?
+        access_key_id = ENV['AWS_ACCESS_KEY_ID']
+      end
+      if !ENV['AWS_SECRET_ACCESS_KEY'].blank?
+        access_key = ENV['AWS_SECRET_ACCESS_KEY']
+      end
+      if !ENV['S3_BUCKET_NAME'].blank?
+        bucket = ENV['S3_BUCKET_NAME']
+      end
+      AWS.config(:access_key_id => access_key_id, :secret_access_key => access_key)
+      s3 = AWS::S3.new
 
-    array = avatar_url.split(bucket+'/')
-    if array.count>1
-      s3_bucket = s3.buckets[bucket]
-      if s3_bucket
-        object = s3_bucket.objects[array.last]
-        if object
-          object.delete
+      array = avatar_url.split(bucket+'/')
+      if array.count>1
+        s3_bucket = s3.buckets[bucket]
+        if s3_bucket
+          object = s3_bucket.objects[array.last]
+          if object
+            object.delete
+          end
+        end
+      end
+      array = thumb_url.split(bucket+'/')
+      if array.count>1
+        s3_bucket = s3.buckets[bucket]
+        if s3_bucket
+          object = s3_bucket.objects[array.last]
+          if object
+            object.delete
+          end
         end
       end
     end
-    array = thumb_url.split(bucket+'/')
-    if array.count>1
-      s3_bucket = s3.buckets[bucket]
-      if s3_bucket
-        object = s3_bucket.objects[array.last]
-        if object
-          object.delete
-        end
-      end
-    end
+    # :nocov:
   end
   private
     # :nocov:
