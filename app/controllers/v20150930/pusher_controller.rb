82d535b7a1c90848026b00452803876597158b7a
Liu, Yang <yang@aglobalway.com>
2015-09-29 15:33:55 -0700

    pusher controller

--- app/controllers/v20150930/pusher_controller.rb
+++ app/controllers/v20150930/pusher_controller.rb
@@ -1,51 +1,60 @@
-class PusherController < ApplicationController
+module V20150930  
+  class PusherController < ApplicationController
+    prepend_before_filter :get_api_token, only: [:auth]
+      
 
+    def auth
+      if current_user
+        auth = Pusher[params[:channel_name]].authenticate(params[:socket_id])
 
-  def auth
-    if current_user
-      auth = Pusher[params[:channel_name]].authenticate(params[:socket_id])
-
-      render :text => params[:callback] + "(" + auth.to_json + ")", :content_type => 'application/javascript'
-    else
-      render :text => "Forbidden", :status => '403'
+        render :text => params[:callback] + "(" + auth.to_json + ")", :content_type => 'application/javascript'
+      else
+        render :text => "Forbidden", :status => '403'
+      end
     end
-  end
 
-  def webhook_channel_exist
-    webhook = Pusher.webhook(request)
-    if webhook.valid?
-      webhook.events.each do |event|
-        case event["name"]
-        when 'channel_occupied'
-          puts "Channel occupied: #{event["channel"]}"
-          channel = event["channel"]
-          if channel.include? "private-user-"
-            channel_array = channel.split "private-user-"
-            if channel_array.count > 1 and channel_array[1].to_i > 0
-              user = User.find_by_id(channel_array[1].to_i)
-              if !user.nil?
-                user.pusher_private_online
+    def webhook_channel_exist
+      webhook = Pusher.webhook(request)
+      if webhook.valid?
+        webhook.events.each do |event|
+          case event["name"]
+          when 'channel_occupied'
+            puts "Channel occupied: #{event["channel"]}"
+            channel = event["channel"]
+            if channel.include? "private-user-"
+              channel_array = channel.split "private-user-"
+              if channel_array.count > 1 and channel_array[1].to_i > 0
+                user = User.find_by_id(channel_array[1].to_i)
+                if !user.nil?
+                  user.pusher_private_online
+                end
               end
             end
-          end
-        when 'channel_vacated'
-          puts "Channel vacated: #{event["channel"]}"
-          channel = event["channel"]
-          if channel.include? "private-user-"
-            channel_array = channel.split "private-user-"
-            if channel_array.count > 1 and channel_array[1].to_i > 0
-              user = User.find_by_id(channel_array[1].to_i)
-              if !user.nil?
-                user.pusher_private_offline
+          when 'channel_vacated'
+            puts "Channel vacated: #{event["channel"]}"
+            channel = event["channel"]
+            if channel.include? "private-user-"
+              channel_array = channel.split "private-user-"
+              if channel_array.count > 1 and channel_array[1].to_i > 0
+                user = User.find_by_id(channel_array[1].to_i)
+                if !user.nil?
+                  user.pusher_private_offline
+                end
               end
             end
           end
         end
+        render text: 'ok'
+      else
+        render text: 'invalid', status: 401
       end
-      render text: 'ok'
-    else
-      render text: 'invalid', status: 401
+    end
+
+    private
+    def get_api_token
+      if (Rails.env != 'test' && api_token = params[:token].blank? && request.headers["X-API-TOKEN"])
+        params[:token] = api_token 
+      end
     end
   end
-
 end
