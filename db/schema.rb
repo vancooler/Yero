c46f4deea419cf687b16e52d404a8a115a4fd3c7
Liu, Yang <yang@aglobalway.com>
2015-10-01 16:42:57 -0700

    points of user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001114638) do
+ActiveRecord::Schema.define(version: 20151001163529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -515,6 +515,7 @@ ActiveRecord::Schema.define(version: 20151001114638) do
     t.string   "version"
     t.boolean  "pusher_private_online",                 default: false
     t.string   "username"
+    t.integer  "point",                                 default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
