9c5da074676a1617eb7b88c59c4d664a19220b95
Liu, Yang <yang@aglobalway.com>
2015-10-19 15:46:30 -0700

    change it

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -322,7 +322,7 @@ class Shout < ActiveRecord::Base
             actions = ["undo_downvote", "upvote"]
         end
 	    json.actions		actions
-        json.shout_comments ShoutComment.list(current_user, shout.id, nil, nil).length
+        json.shout_comments ShoutComment.list(current_user, shout.id, nil, nil)['shout_comments'].length
         json.author_id		shout.user_id
         json.author_username 		(User.find_by_id(shout.user_id).nil? ? "" : User.find_by_id(shout.user_id).username)
         json.network_gimbal_key       ((shout.venue.nil? or shout.venue.beacons.empty?) ? '' : shout.venue.beacons.first.key)
