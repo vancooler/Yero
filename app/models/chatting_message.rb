57ad4a33f87afc5a481b0beb95e1b0a2ae954fad
Liu, Yang <yang@aglobalway.com>
2015-10-26 13:50:15 -0700

    Chatting messages using silent notifications

--- app/models/chatting_message.rb
+++ app/models/chatting_message.rb
@@ -6,7 +6,7 @@ class ChattingMessage < ActiveRecord::Base
   def send_push_notification_to_target_user(message, sender_id, receiver_id)
 	deep_link = "yero://whispers/" + sender_id.to_s
 
-    data = { :alert => message, :type => 2, :badge => "Increment", :deep_link => deep_link}
+    data = { :alert_message => message, :type => 2, :badge => "Increment", :deep_link => deep_link}
     push = Parse::Push.new(data, "User_" + receiver_id.to_s)
     push.type = "ios"
     begin  
